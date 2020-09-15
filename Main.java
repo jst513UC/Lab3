9639c81974517b7f975c30a4e6a17d1aa44074fe
thoma4j3 <thoma4j3@mail.uc.edu>
2020-09-15 23:06:56 +0000

    Created a countdown program.

--- Main.java
+++ Main.java
@@ -3,6 +3,38 @@ class Main
 {
   public static void main(String[] args) 
   {
-    System.out.println("Hello world!");
+    Scanner s = new Scanner(System.in);
+
+    int countDown;
+
+    
+    System.out.println("Enter a number to count down or up from:");
+    countDown = s.nextInt();
+    System.out.println("Blast Off in...");
+    
+    
+    if (countDown >= 0)
+    {
+      while (countDown > 0)
+      {
+       System.out.println(countDown);
+        countDown--;       
+      }
+    }
+    else if (countDown < 0)
+    {
+      while (countDown < 0)
+     {
+        System.out.println(countDown);
+        countDown++;
+     }
+    }
+      
+
+    if (countDown == 0)
+    {
+      System.out.println(countDown);
+      System.out.println("Blast Off!");
+    }
   }
 }
